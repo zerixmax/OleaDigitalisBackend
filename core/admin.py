0a4b1169cbe40aadc045148671a4bd94e28dc0b3
z3r1x <zerix.m@gmail.com>
2025-06-14 19:52:38 +0200

    Dodani model Maslina i registracija u adminu

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Maslina
 
-# Register your models here.
+admin.site.register(Maslina)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Maslina(models.Model):
+    naziv = models.CharField(max_length=100)
+    qr_kod = models.CharField(max_length=100, unique=True)
+    gps_lat = models.FloatField()
+    gps_lon = models.FloatField()
+    datum_unosa = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.naziv} ({self.qr_kod})"
